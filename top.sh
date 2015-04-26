564a8f571f2ae2464861d72a70d736f5b2c77db0
chgangaraju <gangaraju777@gmail.com>
2015-04-26 17:31:31 +0530

    Replaced top with ps and installing the packages

--- top.sh
+++ top.sh
@@ -6,10 +6,10 @@ echo "*********** CPU Usage ****************"
 if [[ ! -z $USER ]] && [[ ! -z $HOST ]] 
 	then
 ssh $USER@$HOST <<EOF
-top -b -n 1 -o %CPU | awk '{print \$1,\$2,\$9,\$12}' | head -n $((MAX_RESULTS+7)) | tail -$((MAX_RESULTS+1))
+ps axo pcpu,comm,pid,euser | sort -nr | head -n $MAX_RESULTS
 EOF
 else
-	top -b -n 1 -o %CPU | awk '{print $1,$2,$9,$12}' | head -n $((MAX_RESULTS+7)) | tail -$((MAX_RESULTS+1))
+	ps axo pcpu,comm,pid,euser | sort -nr | head -n $MAX_RESULTS
 fi	
 }
 
@@ -18,19 +18,45 @@ echo "*********** Memory Usage ****************"
 if [[ ! -z $USER ]] && [[ ! -z $HOST ]] 
 	then
 ssh $USER@$HOST <<EOF
-top -b -n 1 -o %MEM | awk '{print \$1,\$2,\$10,\$12}' | head -n $((MAX_RESULTS+7)) | tail -$((MAX_RESULTS+1))
+ps axo %mem,comm,pid,euser | sort -nr | head -n $MAX_RESULTS
 EOF
 else
-	top -b -n 1 -o %MEM | awk '{print $1,$2,$10,$12}' | head -n $((MAX_RESULTS+7)) | tail -$((MAX_RESULTS+1))
+	ps axo %mem,comm,pid,euser | sort -nr | head -n $MAX_RESULTS
 fi	
 }
 
+install_package() {
+if ! which $1 >/dev/null; then
+	PKG_MANAGER=$( command -v  yum || command -v apt-get ) || echo "Neither yum nor apt-get found"
+	sudo $PKG_MANAGER install -y $1
+fi
+}
+
+
+#disk_io_without_iotop() {
+# Read info from /proc/<pid>/io
+
+# get the difference of rchar,wchar between some interval and sort them
+
+# Get process based on pid
+
+# Print top 10
+#}
+
 print_top_n_disk_io_consumers() {
 echo "*********** Disk I/O Usage ****************"
+
 if [[ ! -z $USER ]] && [[ ! -z $HOST ]] 
 	then
+	ssh -tq $USER@$HOST "
+if ! which iotop >/dev/null; then 
+	PKG_MANAGER=\$( command -v  yum || command -v apt-get ) || echo \"Neither yum nor apt-get found\" 
+	sudo \$PKG_MANAGER install -y iotop 
+	exit
+fi "
 ssh  -t $USER@$HOST "sudo iotop -kqP | head -n $((MAX_RESULTS+3)) | tail -$((MAX_RESULTS+1))"
 else
+	install_package iotop
 	sudo iotop -kqP | head -n $((MAX_RESULTS+3)) | tail -$((MAX_RESULTS+1))
 fi	
 }
@@ -43,24 +69,23 @@ help() {
 while getopts ":u:h:s:n:" option; do
     case "${option}" in
         u)
-            USER=${OPTARG}
-            ;;
+		USER=${OPTARG}
+		;;
         h)
-            HOST=${OPTARG}
-            ;;
+		HOST=${OPTARG}
+		;;
         s)
-			SORT_BY=${OPTARG}
-            ;;
+		SORT_BY=${OPTARG}
+		;;
         n)
-			MAX_RESULTS=${OPTARG}
-            ;;
+		MAX_RESULTS=${OPTARG}
+		;;
         *)
-            help
-            ;;
+		help
+		;;
     esac
 done
 shift $((OPTIND-1))
-
 
 if [[ ${SORT_BY^^} = "MEM" ]] 
 	then
@@ -71,4 +96,3 @@ elif [[ ${SORT_BY^^} = "IO" ]]
 else
 	print_top_n_cpu_consumers
 fi
-
